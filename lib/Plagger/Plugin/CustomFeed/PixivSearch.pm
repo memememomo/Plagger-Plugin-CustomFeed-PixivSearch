21ffcf31fc94db5559fa8662d502b7ca2c073ff8
memememomo <memememomo@gmail.com>
2011-10-21 01:29:09 +0900

    initial commit

---
+++ lib/Plagger/Plugin/CustomFeed/PixivSearch.pm
@@ -0,0 +1,32 @@
+package Plagger::Plugin::CustomFeed::PixivSearch;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plagger::Plugin::CustomFeed::PixivSearch -
+
+=head1 SYNOPSIS
+
+  use Plagger::Plugin::CustomFeed::PixivSearch;
+
+=head1 DESCRIPTION
+
+Plagger::Plugin::CustomFeed::PixivSearch is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
